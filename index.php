13de830c0d97d9d2d774e99f2e0edb640390d5e7
joaryneto <joary@ectecnologia.com.br>
2021-06-24 13:53:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?
 
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-
-
 require_once("./load/load.php");
 
     if($_SESSION['tipo'] == 1)
